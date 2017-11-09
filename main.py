57b26d41570e22e1d99310dd79b5a2860517574e
Linus <linuzlp@gmail.com>
2017-11-09 23:37:22 +0100

    First working version. Added optimized mode for slower PCs.

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import face_recognition
 import cv2
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(1)
 
 face_locations = []
 render_faces = True
+optimized_mode = False
 
 while True:
     ret, frame = video_capture.read()
 
     if render_faces:
-        # Compresses frame for faster processing
-        # compressed_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
-        face_locations = face_recognition.face_locations(frame)
+        # Compresses frame for faster processing (better framerate)
+        # Downscale by 4
+        compressed_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
+        face_locations = face_recognition.face_locations(compressed_frame)
 
     for (top, right, bottom, left) in face_locations:
-        print(top, right, bottom, left)
-        cv2.rectangle(frame, (left,top), (right, bottom), (0, 0, 255), 2)
+        # Multiply by 4 since we downscaled by 4 on the face locator
+        cv2.rectangle(frame, (left*4,top*4), (right*4, bottom*4), (0, 0, 255), 2)
 
     cv2.imshow('Video', frame)
 
     # Switch for every other frame
-    render_faces = not render_faces
+    if optimized_mode:
+        render_faces = not render_faces
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
