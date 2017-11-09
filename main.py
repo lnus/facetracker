578e41d705dbd47b44154df9767c5bb813bbde3a
Linus Wähler <linuzlp@gmail.com>
2017-11-09 16:33:29 +0100

    Optimizing sum shit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 
 video_capture = cv2.VideoCapture(0)
 
+face_locations = []
 render_faces = True
 
 while True:
@@ -11,12 +12,11 @@ while True:
     if render_faces:
         # Compresses frame for faster processing
         compressed_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
-
         face_locations = face_recognition.face_locations(compressed_frame)
 
-        for (top, right, bottom, left) in face_locations:
-            print(top, right, bottom, left)
-            cv2.rectangle(frame, (left,top), (right, bottom), (0,0, 255), 2)
+    for (top, right, bottom, left) in face_locations:
+        print(top, right, bottom, left)
+        cv2.rectangle(frame, (left,top), (right, bottom), (0,0, 255), 2)
 
     cv2.imshow('Video', frame)
 
