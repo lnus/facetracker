eed2381232227e49164c49f9ee843ee35fe7f544
Linus <linuzlp@gmail.com>
2017-11-10 00:12:21 +0100

    Added codec comparing, to differentiate between different faces.

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import cv2
 
 video_capture = cv2.VideoCapture(1)
 
+# For testing
+image_of_me = face_recognition.load_image_file("images/me.jpg") # Place your own file here :)
+my_face_encoding = face_recognition.face_encodings(image_of_me)[0]
+
 face_locations = []
+face_encodings = []
+face_names = []
 render_faces = True
-optimized_mode = False
+optimized_mode = True
+use_face_encodings = True # Detects who is in the frame, slows down the software by like 99%
 
 while True:
     ret, frame = video_capture.read()
@@ -20,9 +27,19 @@ while True:
         # Downscale by 4
         compressed_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
         face_locations = face_recognition.face_locations(compressed_frame)
+        face_names = []
+        if use_face_encodings:
+            face_encodings = face_recognition.face_encodings(compressed_frame, face_locations)
+            for face_encoding in face_encodings:
+                # Look for a match
+                match = face_recognition.compare_faces([my_face_encoding], face_encoding)
+                name = "Unknown"
+                if match[0]:
+                    name = "Me"
+                face_names.append(name)
 
     # Draw the results
-    for (top, right, bottom, left) in face_locations:
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Multiply by 4 since we downscaled by 4 on the face locator
         top *= 4
         right *= 4
@@ -33,12 +50,13 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (255, 255, 255), 5)
 
         # Draw the name below the face
-        name = "???" # Placeholder
+        if not name: name = "Unknown" # In case 'use_face_encodings' is False
         cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (255, 255, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left+6, bottom), font, 1.0, (0,0,0), 1)
-
+        
     cv2.imshow('Video', frame)
+
 
     # Switch for every other frame
     if optimized_mode:
