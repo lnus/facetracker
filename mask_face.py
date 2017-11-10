70a9990c17a21ab504cbfb3b42a20f8996a7d4a8
Linus Wähler <linuzlp@gmail.com>
2017-11-10 15:07:28 +0100

    Added toggle for compression

--- mask_face.py
+++ mask_face.py
@@ -6,13 +6,16 @@ import random
 video_capture = cv2.VideoCapture(0)
 
 face_landmarks_list = []
+compressed_mode = False # False = Better accuracy, True = Faster speeds
 
 while True:
     ret, frame = video_capture.read()
     frame = cv2.flip(frame, 1)
 
+    compute_frame = frame
     compressed_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
-    face_landmarks_list = face_recognition.face_landmarks(compressed_frame)
+    if compressed_mode: compute_frame = compressed_frame
+    face_landmarks_list = face_recognition.face_landmarks(compute_frame)
 
     for face_landmarks in face_landmarks_list:
         facial_features = [
@@ -32,7 +35,10 @@ while True:
             # Scales up the points with 4, since the image was scaled down by 4
             points = []
             for point in face_landmarks[facial_feature]:
-                new_point = (point[0]*4, point[1]*4)
+                if compressed_mode:
+                    new_point = (point[0]*4, point[1]*4)
+                else:
+                    new_point = point
                 points.append(new_point)
             for i in range(len(points)-1):
                 cv2.line(frame, points[i], points[i+1], (random.randint(0,255),random.randint(0,255),random.randint(0,255)), 5)
